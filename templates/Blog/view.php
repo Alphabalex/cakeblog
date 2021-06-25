740165c297eca2f016a12e9c16a95a2f6ab484a7
Balogun Abdulquddus <babusunnah@gmail.com>
2021-06-25 21:31:58 +0100

    cleaning up

--- templates/Blog/view.php
+++ templates/Blog/view.php
@@ -16,7 +16,6 @@
                             <h4><?= h($post->title) ?></h4>
                             <div class="post-single-info">
                                 <ul class="list-inline">
-                                    <li><a href="author.html"><img src="assets/img/author/1.jpg" alt=""></a></li>
                                     <li><a href="#"><?= h($post->user->lastname) ?> <?= h($post->user->firstname) ?></a> </li>
                                     <li class="dot"></li>
                                     <li><?= h($post->modified) ?></li>
@@ -33,54 +32,6 @@
                                 </div>
                             </div>
                         </div> <!--/-->
-
-                    <!--next & previous-posts-->
-                    <div class="row mt-5">
-                        <div class="col-md-6">
-                            <div class="widget">
-                                <div class="widget-next-post">
-                                    <div class="small-post">
-                                        <div class="image">
-                                            <a href="post-default.html">
-                                                <img src="assets/img/latest/1.jpg" alt="...">
-                                            </a>
-                                        </div>
-                                        <div class="content">
-                                            <div>
-                                                <a class="link" href="post-default.html">
-                                                    <i class="arrow_left"></i>Preview post</a>
-                                            </div>
-                                            <a href="post-default.html">7 Healty Dinner Recipes for a Date Night at Home</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="widget">
-                                <div class="widget-previous-post">
-                                    <div class="small-post">
-                                        <div class="image">
-                                            <a href="post-default.html">
-                                                <img src="assets/img/blog/2.jpg" alt="...">
-                                            </a>
-                                        </div>
-                                        <div class="content">
-                                            <div>
-                                                <a class="link" href="post-default.html">
-                                                    <span> Next post</span>
-                                                    <span class="arrow_right"></span>
-                                                </a>
-                                            </div>
-                                            <a href="post-default.html">How to Choose Outfits for Work for woman and men</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/-->
-
                 </div>
             </div>
         </div>
